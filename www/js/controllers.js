d57b747c7b3193bb05205e57c5be410651d17c23
Ashley Sanders <ashleymsanders2@gmail.com>
2016-12-13 02:02:55 +0200

    My course updates

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -14,16 +14,54 @@ angular.module('starter.controllers', [])
 .controller('MyCoursesCtrl', function($scope) {
 
     $scope.myCourses = [
-        "Milnerton",
-        "Atlantic Beach",
-        "Clovelly",
-        "Metropiton",
-        "Pearl Valley",
-        "River Club",
-        "Steenberg",
-        "Sun City",
-        "Augusta National",
-        "Happy Land"
-    ]
+        {
+            name: "Milnerton",
+            temp: "11°"    
+        },
+         {
+            name: "Atlantic Beach",
+            temp: "16°"    
+        },
+         {
+            name: "Clovelly",
+            temp: "17°"    
+        },
+         {
+            name: "Metropiton",
+            temp: "21°"    
+        },
+         {
+            name: "Pearl Valley",
+            temp: "13°"    
+        },
+         {
+            name: "River Club",
+            temp: "14°"    
+        },
+         {
+            name: "Steenberg",
+            temp: "22°"    
+        },
+         {
+            name: "Sun City",
+            temp: "19°"    
+        },
+         {
+            name: "Augusta National",
+            temp: "18°"    
+        },
+        {
+            name: "Happy Land",
+            temp: "14°"    
+        }
+    ];
+    
+    $scope.selectCourse = function(myCourse, index) {
+        $scope.selectedCourse = myCourse;
+    };
+    
+    $scope.editCourse = function() {
+        $state.go('editCourse');
+    };
     
 })
