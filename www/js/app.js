e7d1886039f65faffb0d2a670c42195703767b11
Cameron Sanders <cameron.w.sanders@gmail.com>
2017-01-10 10:16:31 +0200

    corrected sub_region course retrieval

--- www/js/app.js
+++ www/js/app.js
@@ -748,8 +748,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives']
     service.CoursesForRegion = function(subregionID) {
       var deferred = $q.defer();
 	    var promise = deferred.promise;
+      var url = AppService.GetUrl('get-courses-for-regions/country_id/{countryID}/region_id/{id}}', {countryID: service.selectedCountry.id, id: subregionID});
+      if (subregionID != service.selectedRegion.id)
+      {
+        url = AppService.GetUrl('get-courses-for-regions/country_id/{countryID}/region_id/{regionID}/sub_region_id/{subregionID}', {countryID: service.selectedCountry.id, regionID: service.selectedRegion.id,  subregionID: subregionID});
+      }
       $http({
-        url: AppService.GetUrl('get-courses-for-regions/country_id/{countryID}/region_id/{id}}', {countryID: service.selectedCountry.id, id: subregionID}),
+        url: url,
         method: "GET"
       }).success(function (data, status, headers, config) {
         console.log('CoursesForRegion->success: ', data);
